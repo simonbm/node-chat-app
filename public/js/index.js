a1861382773e2da7a642c40470f069ff1816021f
Simon Boyce-Maynard <sboycemaynard@Simons-MBP-2018.local>
2018-05-04 13:54:36 -0700

    Added inital Chat IU and jQuery Intergration

--- public/js/index.js
+++ public/js/index.js
@@ -22,6 +22,34 @@ socket.on('disconnect', function() {
 //     console.log('New email', email);
 // });
 
- socket.on('newMessage', function(email) {
-     console.log('New Message', email);
+ socket.on('newMessage', function(message) {
+     console.log('New Message', message);
+
+     var li = jQuery('<li></li>');
+     li.text(`${message.from}: ${message.text}`);
+
+     jQuery('#messages').append(li);
+
  });
+
+
+
+// socket.emit('createMessage', {
+//     from: 'Frank',
+//     text: 'Did you see Avengers?'
+// }, function (data)  {
+//     console.log('Got it', data);
+// });
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    },function ()  {
+
+    });
+
+});
